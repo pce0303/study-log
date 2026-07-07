e83e89e741abd79e7998c341e72706a976aff1d4
choeun <ivychoen@gmail.com>
2026-07-07 09:43:08 +0900

    [Passed] 특정 수 도달하기 (1ms, 0MB)

--- trail1/특정 수 도달하기/reaching-specific-number.c
+++ trail1/특정 수 도달하기/reaching-specific-number.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    // Please write your code here.
     int arr[10];
     int sum = 0;
+    int count = 0;
     double avg = 0;
 
     for (int i = 0; i < 10; i++) {
-        scanf(" %d", &arr[i]);
+        scanf("%d", &arr[i]);
+
         if (arr[i] >= 250) {
-            for (int j = 0; j <i; j++) {
-                sum += arr[j];
-            }
-            avg = (double)sum/i;
             break;
         }
+
+        sum += arr[i];
+        count++;
     }
+
+    avg = (double)sum / count;
 
     printf("%d %.1lf", sum, avg);
 
